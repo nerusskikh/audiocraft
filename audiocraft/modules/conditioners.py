c6b66926537e76d945cba2c364ff7ffbc92f4e64
nerusskikh <russkikh.nikolay@gmail.com>
2024-02-07 10:40:48 +0000

    changing accompanement to drums

--- audiocraft/modules/conditioners.py
+++ audiocraft/modules/conditioners.py
@@ -544,7 +544,7 @@ class ChromaStemConditioner(WaveformConditioner):
         self.__dict__['demucs'] = pretrained.get_model('htdemucs_6s').to(device)
         stem_sources: list = self.demucs.sources  # type: ignore
         #self.stem_indices = torch.LongTensor([stem_sources.index('vocals'), stem_sources.index('other')]).to(device)
-        self.stem_indices = torch.LongTensor([stem_sources.index('guitar')]).to(device)
+        self.stem_indices = torch.LongTensor([stem_sources.index('drums')]).to(device)
         self.chroma = ChromaExtractor(sample_rate=sample_rate, n_chroma=n_chroma,
                                       radix2_exp=radix2_exp, **kwargs).to(device)
         self.chroma_len = self._get_chroma_len()
